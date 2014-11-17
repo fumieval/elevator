d35f104642f3fb66eaa7ba94d1ef32e86b557883
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-11-17 12:27:22 +0900

    nice names

--- src/Control/Elevator.hs
+++ src/Control/Elevator.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeOperators, FlexibleContexts, FlexibleInstances, ConstraintKinds, TypeFamilies, DataKinds #-}
+{-# LANGUAGE TypeOperators, FlexibleContexts, DefaultSignatures, FlexibleInstances, ConstraintKinds, TypeFamilies, DataKinds #-}
 module Control.Elevator where
 import Control.Monad.Trans.State.Lazy as Lazy
 import Control.Monad.Trans.State.Strict as Strict
@@ -11,75 +11,84 @@ import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
 import Data.Monoid
+import Control.Monad.ST
 
-class Altitude f where
-  type Ground f :: List (* -> *)
-  smooth :: Union (Ground f) a -> f a
+class Tower f where
+  type Floors f :: List (* -> *)
+  type Floors f = Empty
+  toLoft :: Union (Floors f) a -> f a
+  default toLoft :: Union Empty a -> f a
+  toLoft = exhaust
 
-type Elevate f g = (Altitude g, f ∈ (g :> Ground g))
+type Elevate f g = (Tower g, f ∈ Floors1 g)
+
+type Floors1 g = g :> Floors g
+
+toLoft1 :: Tower f => Union (Floors1 f) a -> f a
+toLoft1 = id ||> toLoft
 
 elevate :: Elevate f g => f a -> g a
-elevate f = (id ||> smooth) (liftU f)
+elevate f = (id ||> toLoft) (liftU f)
 {-# INLINE elevate #-}
 
-instance Altitude (Union u) where
-  type Ground (Union u) = u
-  smooth = id
+instance Tower IO where
+  type Floors IO = ST RealWorld :> Empty
+  toLoft = stToIO ||> exhaust
 
-instance Monad m => Altitude (Lazy.StateT s m) where
-  type Ground (Lazy.StateT s m) = m
-    :> Lazy.State s
+instance Tower Maybe
+instance Tower (Either e)
+instance Tower ((->) r)
+instance Tower []
+instance Tower (ST s)
+
+instance Tower (Union u) where
+  type Floors (Union u) = u
+  toLoft = id
+
+instance (Monad m, Tower m) => Tower (Lazy.StateT s m) where
+  type Floors (Lazy.StateT s m) = Lazy.State s
     :> Strict.State s
     :> Strict.StateT s m
-    :> Empty
-  smooth = lift
-    ||> Lazy.state . Lazy.runState
+    :> Floors1 m
+  toLoft = Lazy.state . Lazy.runState
     ||> Lazy.state . Strict.runState
     ||> Lazy.StateT . Strict.runStateT
-    ||> exhaust
+    ||> lift . toLoft1
 
-instance Monad m => Altitude (Strict.StateT s m) where
-  type Ground (Strict.StateT s m) = m
-    :> Lazy.State s
+instance (Monad m, Tower m) => Tower (Strict.StateT s m) where
+  type Floors (Strict.StateT s m) = Lazy.State s
     :> Strict.State s
     :> Lazy.StateT s m
-    :> Empty
-  smooth = lift
-    ||> Strict.state . Lazy.runState
+    :> Floors1 m
+  toLoft = Strict.state . Lazy.runState
     ||> Strict.state . Strict.runState
     ||> Strict.StateT . Lazy.runStateT
-    ||> exhaust
+    ||> lift . toLoft1
 
-instance Monad m => Altitude (ReaderT r m) where
-  type Ground (ReaderT r m) = m
-    :> Reader r
+instance (Monad m, Tower m) => Tower (ReaderT r m) where
+  type Floors (ReaderT r m) = Reader r
     :> (->) r
-    :> Empty
-  smooth = lift
-    ||> reader . runReader
+    :> Floors1 m
+  toLoft = reader . runReader
     ||> reader
-    ||> exhaust
+    ||> lift . toLoft1
 
-instance (Monoid w, Monad m) => Altitude (Lazy.WriterT w m) where
-  type Ground (Lazy.WriterT s m) = m
-    :> Lazy.Writer s
+instance (Monoid w, Monad m, Tower m) => Tower (Lazy.WriterT w m) where
+  type Floors (Lazy.WriterT s m) = Lazy.Writer s
     :> Strict.Writer s
     :> Strict.WriterT s m
-    :> Empty
-  smooth = lift
-    ||> Lazy.writer . Lazy.runWriter
+    :> Floors1 m
+  toLoft = Lazy.writer . Lazy.runWriter
     ||> Lazy.writer . Strict.runWriter
     ||> Lazy.WriterT . Strict.runWriterT
-    ||> exhaust
+    ||> lift . toLoft1
 
-instance (Monoid w, Monad m) => Altitude (Strict.WriterT w m) where
-  type Ground (Strict.WriterT s m) = m
-    :> Lazy.Writer s
+instance (Monoid w, Monad m, Tower m) => Tower (Strict.WriterT w m) where
+  type Floors (Strict.WriterT s m) = Lazy.Writer s
     :> Strict.Writer s
     :> Lazy.WriterT s m
-    :> Empty
-  smooth = lift
-    ||> Strict.writer . Lazy.runWriter
+    :> Floors1 m
+  toLoft = Strict.writer . Lazy.runWriter
     ||> Strict.writer . Strict.runWriter
     ||> Strict.WriterT . Lazy.runWriterT
-    ||> exhaust
+    ||> lift . toLoft1
